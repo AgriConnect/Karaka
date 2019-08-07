bb8f74dc4f9fa4015378804b23d1616a263e538d
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2019-08-08 00:14:16 +0700

    Add configuration for web server port/socket path

--- runserver.py
+++ runserver.py
@@ -3,26 +3,36 @@
 import logging
 import asyncio
 
+import click
 import logbook
 from logbook import Logger
 from aiogram.utils.executor import Executor
 
 from alarmbot.loghandlers import ColorizedStderrHandler
-from alarmbot.views import app
-from alarmbot.receptionist import dp
 
 
 # Configure logging
 logger = Logger(__name__)
 logger.handlers.append(ColorizedStderrHandler())
-DEFAULT_SOCK_PATH = '/run/alarmbot/api.sock'
 
 
-if __name__ == '__main__':
+@click.command()
+@click.option('-s', '--socket', type=click.Path())
+@click.argument('port', default=8006, type=int)
+def main(socket, port):
+    from alarmbot.views import app
+    from alarmbot.receptionist import dp
     logger.level = logbook.DEBUG
     logging.basicConfig(level=logging.INFO)
     loop = asyncio.get_event_loop()
     executor = Executor(dp, skip_updates=True, loop=loop)
     logger.info('{}', executor)
     executor.set_webhook(web_app=app)
-    executor.run_app(path=DEFAULT_SOCK_PATH)
+    if not socket:
+        executor.run_app(port=port)
+    else:
+        executor.run_app(path=socket)
+
+
+if __name__ == '__main__':
+    main()
